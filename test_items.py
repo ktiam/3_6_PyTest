3f40093c6e4e9619ded6d4a61639f12da4a1ab8f
ktiam <ktiam80@rambler.ru>
2021-02-02 19:37:40 +0300

    add test_items.py

--- test_items.py
+++ test_items.py
@@ -6,7 +6,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_button_add_to_basket(browser):
     browser.get(link)
-    time.sleep(30)
     try:
         btn = browser.find_element_by_css_selector(".btn.btn-lg.btn-primary.btn-add-to-basket")
     except (NoSuchElementException):
